0c0c1ae658190a09ab6dd62214c959659c263a97
chien-ying <b06505032@ntu.edu.tw>
2022-12-03 15:49:29 -0500

    link client with server

--- client/client.py
+++ client/client.py
@@ -1,14 +1,23 @@
 from fetchImg import *
+import requests
 
 numOfImage = 5
 
 images = []
 
+imageFolder = "client/image"
 
 
 if __name__ == '__main__':
     images = fetchImages(numOfImage)
 
+    url = 'http://127.0.0.1:5000/recognize'
     for i in range(numOfImage):
-        images[i].show()
+        # images[i].show()
+
+        my_img = {'image': open(f"{imageFolder}/{i+1}.png", 'rb')}
+        r = requests.post(url, files=my_img)
+
+        # convert server response into JSON format.
+        print(r.json())
 
