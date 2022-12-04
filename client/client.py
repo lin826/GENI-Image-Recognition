2a5ab9b4356d8b3232d511568bad54d26bdf04df
chien-ying <b06505032@ntu.edu.tw>
2022-12-03 19:43:27 -0500

    add parser in client

--- client/client.py
+++ client/client.py
@@ -1,14 +1,22 @@
 from fetchImg import *
 import requests
-
-numOfImage = 5
+import argparse
 
 imageFolder = "image"
 
 url = 'http://127.0.0.1:5000/recognize'
 
 
+def parseOption():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-s", "--server", type=str, required=True, help="server address")
+    parser.add_argument("-n", "--num", type=int, help="number of image", default=5)
+    args = parser.parse_args()
+    return args.server, args.num
+
+
 if __name__ == '__main__':
+    server, numOfImage = parseOption()
     # images = fetchImages(imageFolder, numOfImage)
     
     for i in range(numOfImage):
@@ -22,7 +30,7 @@ if __name__ == '__main__':
             my_img = {'image': open(f"{imageFolder}/{i+1}.png", 'rb')}
             print("image fetch from url")
 
-        r = requests.post(url, files=my_img)
+        r = requests.post(server, files=my_img)
         # convert server response into JSON format.
         print(r.json())
 
