88543053b84bedb9a732243606aeae22f46f3691
cy <cy@server.cy-test.ch-geni-net.instageni.research.umich.edu>
2022-12-07 21:56:57 -0700

    Add time stamp in server

--- server/server.py
+++ server/server.py
@@ -7,6 +7,8 @@ import torch
 from PIL import Image
 from flask import Flask, request, jsonify
 from torchvision.datasets import CIFAR100
+from datetime import datetime
+
 
 # Load the model
 device = "cuda" if torch.cuda.is_available() else "cpu"
@@ -23,6 +25,7 @@ def index():
 
 @app.route('/recognize', methods=['POST'])
 def recognize_image():
+    startTime = datetime.now().strftime("%H:%M:%S.%f")
     file = request.files['image']
     img = Image.open(file.stream)
 
@@ -40,8 +43,14 @@ def recognize_image():
     similarity = (100.0 * image_features @ text_features.T).softmax(dim=-1)
     values, indices = similarity[0].topk(1)
 
+    endTime = datetime.now().strftime("%H:%M:%S.%f")
+    delta = datetime.strptime(endTime, "%H:%M:%S.%f") - datetime.strptime(startTime, "%H:%M:%S.%f")
+    processTime = delta.total_seconds() * 1000
     for value, index in zip(values, indices):
-        return jsonify({'text': cifar100.classes[index],'accuracy': value.item()})
+        return jsonify({'text': cifar100.classes[index],
+        'accuracy': value.item(), 
+        'server process time': processTime
+    })
 
 if __name__ == '__main__':
     app.run()
